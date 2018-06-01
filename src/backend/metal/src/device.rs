42456a82f5b812c76b6decbd225e845459d05f26
Dzmitry Malyshau <kvarkus@gmail.com>
2018-05-31 22:43:07 -0400

    [mtl] fix mag filter and address modes

--- src/backend/metal/src/device.rs
+++ src/backend/metal/src/device.rs
@@ -1098,7 +1098,7 @@ impl hal::Device<Backend> for Device {
         let descriptor = metal::SamplerDescriptor::new();
 
         descriptor.set_min_filter(conv::map_filter(info.min_filter));
-        descriptor.set_mag_filter(conv::map_filter(info.min_filter));
+        descriptor.set_mag_filter(conv::map_filter(info.mag_filter));
         descriptor.set_mip_filter(match info.mip_filter {
             image::Filter::Nearest => MTLSamplerMipFilter::Nearest,
             image::Filter::Linear => MTLSamplerMipFilter::Linear,
@@ -1108,10 +1108,10 @@ impl hal::Device<Backend> for Device {
             descriptor.set_max_anisotropy(aniso as _);
         }
 
-        let (r, s, t) = info.wrap_mode;
-        descriptor.set_address_mode_r(conv::map_wrap_mode(r));
+        let (s, t, r) = info.wrap_mode;
         descriptor.set_address_mode_s(conv::map_wrap_mode(s));
         descriptor.set_address_mode_t(conv::map_wrap_mode(t));
+        descriptor.set_address_mode_r(conv::map_wrap_mode(r));
 
         descriptor.set_lod_bias(info.lod_bias.into());
         descriptor.set_lod_min_clamp(info.lod_range.start.into());
